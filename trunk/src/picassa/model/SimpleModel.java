9fa48bf17b72fd4c740bf3a6a541c392e7e03f8c
ascripa@gmail.com <ascripa@gmail.com@62d7d3bf-8d4b-596a-aef5-d36424b0c208>
2011-02-20 00:05:03 +0000

    Working Fractals, limited color selection...

--- trunk/src/picassa/model/SimpleModel.java
+++ trunk/src/picassa/model/SimpleModel.java
@@ -3,11 +3,16 @@
  */
 package picassa.model;
 
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 import picassa.model.expression.Expression;
 import picassa.model.parser.SimpleParser;
+import picassa.util.Complex;
 import picassa.util.Pixmap;
 import picassa.util.RGBColor;
 import util.parser.ParserException;
@@ -18,14 +23,106 @@ import util.parser.ParserException;
  */
 public class SimpleModel extends AbstractModel
 {
-    public static final double DEFAULT_DOMAIN_MIN = -1;
-    public static final double DEFAULT_DOMAIN_MAX = 1;
+    private static final Color[] COLOR_MAP = new Color[4];
+    
+    static
+    {    
+    COLOR_MAP[0] = new Color(0, 0, 0);
+    COLOR_MAP[1] = new Color(176, 31, 23);
+    COLOR_MAP[2] = new Color(220, 60, 20);
+    COLOR_MAP[3] = new Color(255, 255, 255);
+    }
+    
+    /**
+    private static String colorFile = 
+        "/data/ColorMap.txt";
+    private static final File colors = new File(colorFile);
 
+    static
+    {
+        Scanner in = null;    
+        try
+            {
+                in = new Scanner(colors);
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Could not open color file.");
+                e.printStackTrace();
+            }
+            
+          int i = 0;
+            while (in.hasNext() && i<4)
+            {
+                COLOR_MAP[i] = new Color(in.nextInt(),in.nextInt(), in.nextInt());
+            }
+    }
+    */
+    
+    
+    public static final double DEFAULT_DOMAIN_MAX = 1;
+    public static final double DEFAULT_DOMAIN_MIN = -1;
+    private static final int MAX_ITERATIONS = 100;
+    private static final int RANGE = 1;
+
+
+    public Pixmap fractalizeExpression (Expression expression, Pixmap baseImage)
+    {
+        Pixmap fractalImage = new Pixmap(baseImage);
+        int height = baseImage.getSize().height;
+        int width = baseImage.getSize().width;
+        Map<String, Number> variables = new HashMap<String, Number>();
+        
+        for (int yAxis = 0; yAxis < height; yAxis++)
+        {
+            double y = imageToDomainScale(yAxis, height);
+            for (int xAxis = 0; xAxis < width; xAxis++)
+            {
+                double x = imageToDomainScale(xAxis, width);
+                variables.put("x", x);
+                variables.put("y", y); 
+            
+            //x and y are the locations of the pixel in the pixmap
+            //a complex number is created with x the real component and y the imaginary one.
+  
+
+            int counter = 0;
+            while ((Math.sqrt((x * x) + (y * y)) < RANGE) &&
+                   (counter <= MAX_ITERATIONS))
+            {
+                List<Number> values = expression.evaluate(variables);
+                if (values.size()==1)
+                {
+                    x = values.get(0).doubleValue();
+                    y = x;
+                }
+                else if (values.size()>=2)
+                {
+                    x = values.get(0).doubleValue();
+                    y = values.get(1).doubleValue();
+                }
+                else throw new RuntimeException("Fractalize failed: " + values.toString());
+                counter++;
+            }
+
+            //Pixel's color depends on how many iterations it took x and y to "escape"
+            Color pixelColor = COLOR_MAP[counter % 4];
+            fractalImage.setColor(xAxis, yAxis, pixelColor);
+       
+            }
+        }
+        return fractalImage;
+    }
+
+
+    //This could be more exciting but I kept it simple because...
+    //1: It already took a pretty long time to research and make this, and
+    //2: 8 minutes is considered a "fast" runtime for a fractal algorithm.
 
     /**
      * Convert from image space to default domain space.
      */
-    protected double imageToDomainScale (int value, int range)
+    private double imageToDomainScale (int value, int range)
     {
         return imageToDomainScale(value,
                                   range,
@@ -57,7 +154,6 @@ public class SimpleModel extends AbstractModel
     {
         return SimpleParser.parse(expression);
     }
-
 
     /**
      * @see picassa.model.AbstractModel#renderExpression(picassa.model.expression.Expression,
