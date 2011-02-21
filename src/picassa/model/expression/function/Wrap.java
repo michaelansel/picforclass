eb85d82dfb6c073c509ba1f276eaa9f66e180a75
maxeganj <maxeganj@62d7d3bf-8d4b-596a-aef5-d36424b0c208>
2011-02-21 05:03:42 +0000

    Some functions

--- src/picassa/model/expression/function/Wrap.java
+++ src/picassa/model/expression/function/Wrap.java
@@ -1,5 +1,39 @@
 package picassa.model.expression.function;
 
-public class Wrap {
+import java.util.List;
 
+import picassa.model.expression.Expression;
+
+/**
+ * @author Max Egan
+ */
+
+public class Wrap extends OneArgFunction
+{
+	public final static String FUNCTION_NAME = "clamp";
+
+	
+	 public Wrap(String functionName, List<Expression> parameters) {
+		super(functionName, parameters);
+	}
+
+
+	    @Override
+	    protected Number evaluateValue (Number value)
+	    {
+
+	        if(value.floatValue() < -1)
+	        {
+	        	float floatValue = value.floatValue();
+	        	float coreValue = floatValue %1;
+	        	return floatValue - coreValue;
+	        }
+	        if (value.floatValue() > 1)
+	        {
+	        	float floatValue = value.floatValue();
+	        	float coreValue = floatValue %1;
+	        	return floatValue - coreValue;
+	        }
+	        return value;
+	    }
 }
