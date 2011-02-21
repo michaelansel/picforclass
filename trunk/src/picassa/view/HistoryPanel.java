ea36e809c93faf9b232444aa639ea2258f151929
ascripa@gmail.com <ascripa@gmail.com@62d7d3bf-8d4b-596a-aef5-d36424b0c208>
2011-02-21 09:09:20 +0000

    Thumbnail (singular); can't figure out why it won't show all of them...

--- trunk/src/picassa/view/HistoryPanel.java
+++ trunk/src/picassa/view/HistoryPanel.java
@@ -1,17 +1,17 @@
 package picassa.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.util.ResourceBundle;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
-import picassa.util.Pixmap;
+import javax.swing.border.TitledBorder;
 
 
 /**
@@ -23,11 +23,13 @@ public class HistoryPanel extends JPanel
     private static final long serialVersionUID = 1L;
     private Dimension thumbnailSize;
     private int historyLength = 10;
+    private JFrame myContainer;
 
 
     public HistoryPanel (JFrame container, Dimension size)
     {
         super(new BorderLayout());
+        myContainer = container;
         JPanel historyPanel = new JPanel();
         historyPanel.setLayout(new BoxLayout(historyPanel, BoxLayout.X_AXIS));
         JScrollPane scrollpane =
@@ -38,27 +40,38 @@ public class HistoryPanel extends JPanel
         add(scrollpane);
         setPreferredSize(size);
         thumbnailSize = new Dimension((size.height - 40), (size.height - 40));
-        
-        //addThumbnails(this);
+
+        addThumbnails(this);
     }
-    
+
+
     private void addThumbnails (HistoryPanel historyPanel)
     {
         ResourceBundle myResources =
             ResourceBundle.getBundle("picassa.resources.view");
-        
-        for (int k=0; k<historyLength; k++)
+
+        historyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+        for (int k = 0; k < historyLength; k++)
         {
-            String historyNumber = "History" + (k+1);
-            Pixmap thumbnail = createThumbnail(myResources.getString(historyNumber));
-            //historyPanel.add(thumbnail);
+            String historyNumber = "History" + k;
+            Thumbnail thumbnail = createThumbnail();
+            historyPanel.add(thumbnail);
+            historyPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+            TitledBorder title =
+                BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.black),
+                                                 myResources.getString(historyNumber));
+            title.setTitleJustification(TitledBorder.CENTER);
+            title.setTitlePosition(TitledBorder.BOTTOM);
+            thumbnail.setBorder(title);
         }
-        
+
     }
 
-    private Pixmap createThumbnail (String historyNumber)
+
+    private Thumbnail createThumbnail ()
     {
-        Pixmap currentThumbnail = new Pixmap(thumbnailSize);
+        Thumbnail currentThumbnail = new Thumbnail(myContainer, thumbnailSize);
         return currentThumbnail;
     }
 
